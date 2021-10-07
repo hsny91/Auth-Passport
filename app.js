d57433c3e6afaa42d22a54acd3e2920deef70bab
hsny91 <husniyesakmak.91@gmail.com>
2021-10-07 13:53:20 +0200

    views folder was created

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 // MONGODB PASSPORT
 // Istanbul1991-
 const express = require('express')
+var exphbs  = require('express-handlebars');
 const app = express();
 const PORT = 3000 || process.env.PORT;
 const mongoose = require("./db/mongoose");
+const userRouter = require('./router/user') 
 
+//Template Engine Middleware
 
+app.engine('handlebars', exphbs({defaultLayout:'mainLayout'}));
+app.set('view engine', 'handlebars');
+
+// Router middleware
+app.use(userRouter);
 
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World!')
+  res.send('Home Page !')
+})
+
+app.use((req,res,next)=>{
+  res.send('404 Not Found')
 })
 
 app.listen(PORT, () => {
